026573da4f32d1ed8bf7ab432854c85cfe3a8228
diega <diegochiola@gmail.com>
2024-05-06 16:24:03 +0200

    interaction 5: done

--- src/App.vue
+++ src/App.vue
@@ -10,22 +10,42 @@ const fiveContacts = ref(contactsList.value.slice(0, 5));
 
 //interaction 3
 const addRandomContact = () => {
-  const notDisplayedContacts = contactsList.value.filter(contact => !fiveContacts.value.includes(contact));
+  const notDisplayedContacts = contactsList.value.filter(
+    (contact) => !fiveContacts.value.includes(contact)
+  );
   if (notDisplayedContacts.length > 0) {
     const randomIndex = Math.floor(Math.random() * notDisplayedContacts.length);
     fiveContacts.value.push(notDisplayedContacts[randomIndex]);
-  }else{
+  } else {
     alert("Sorry! No more contacts to display");
   }
 };
 
+//interaction 4
+const sortByPopularity = () => {
+  fiveContacts.value.sort((a, b) => b.popularity - a.popularity);
+};
+const sortByName = () => {
+  fiveContacts.value.sort((a, b) => a.name.localeCompare(b.name));
+}
+
+//interaction 5
+const removeContact = (index) => {
+  fiveContacts.value.splice(index, 1);
+};
 
 </script>
+
 
 <template>
   <section>
     <h1>Iron-Contacts</h1>
-    <button @click="addRandomContact">Add Random Contact</button>
+    <div>
+      <button @click="addRandomContact">Add Random Contact</button>
+      <button @click="sortByPopularity">Sort By Popularity</button>
+      <button @click="sortByName">Sort By Name</button>
+    </div>
+
     <table class="table">
       <tr>
         <th>Picture</th>
@@ -33,14 +53,18 @@ const addRandomContact = () => {
         <th>Popularity</th>
         <th>Won an Oscar</th>
         <th>Won an Emmy</th>
+        <th>Actions</th>
       </tr>
-      <tr v-for="contact in fiveContacts">
+      <tr v-for="(contact, index) in fiveContacts" :key="index">
         <!-- otra forma <tr v-for="(contact, index) in contactsList" :key="index" v-if="index < 5"> -->
         <td><img :src="contact.pictureUrl" /></td>
         <td>{{ contact.name }}</td>
-        <td>{{ contact.popularity }}</td>
+        <td>{{ contact.popularity.toFixed(2) }}</td>
         <td>{{ contact.wonOscar ? "🏆" : "" }}</td>
         <td>{{ contact.wonEmmy ? "🏆" : "" }}</td>
+        <td>
+          <button @click="removeContact(index)">Delete</button>
+        </td>
       </tr>
     </table>
   </section>
@@ -53,7 +77,7 @@ section {
   align-items: center;
   justify-content: center;
 }
-button{
+button {
   margin: 20px;
   padding: 10px;
   border: none;
